a6ce0df10d62b6d47f723107452cc89fb9d4ea0c
unknown <nawalhemed@gmail.com>
2019-07-30 17:14:37 -0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import 'App.dart';
+
+
+
+void main() => runApp(App());
+
+
+
---
+++ lib/views/pages/login_page.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            SizedBox(height: 250,),
+            Center(child: CircleAvatar(radius: 70,
+            backgroundColor: Colors.transparent,
+            child: Image(image: AssetImage('assets/icons/diamond.png'),
+            height: 120),),),
+            SizedBox(height: 70,),
+            Text('TeamUP',
+                style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                )),
+            Text('The app tagline goes here',
+                style: TextStyle(
+                  fontSize: 10,
+                ))
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+          color: Colors.purple,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              FlatButton(
+                child: Text(
+                  'LOG IN',
+                  style: TextStyle(color: Colors.white),
+                ),
+                onPressed: () {},
+              ),
+              FlatButton(
+                child: Text(
+                  'SIGN UP',
+                  style: TextStyle(color: Colors.white),
+                ),
+                onPressed: () {},
+              )
+            ],
+          )),
+    );
+  }
+}
