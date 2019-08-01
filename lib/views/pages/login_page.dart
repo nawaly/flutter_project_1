654067f4da8e2ef6a7d8289765fb6d53a4cb02f1
unknown <nawalhemed@gmail.com>
2019-08-01 09:54:01 -0700

    login,signup&sign pages

--- lib/views/pages/login_page.dart
+++ lib/views/pages/login_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:pt_project_1/contants/constant.dart';
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -7,12 +7,20 @@ class LoginPage extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           children: <Widget>[
-            SizedBox(height: 250,),
-            Center(child: CircleAvatar(radius: 70,
-            backgroundColor: Colors.transparent,
-            child: Image(image: AssetImage('assets/icons/diamond.png'),
-            height: 120),),),
-            SizedBox(height: 70,),
+            SizedBox(
+              height: 250,
+            ),
+            Center(
+              child: CircleAvatar(
+                radius: 70,
+                backgroundColor: Colors.transparent,
+                child: Image(
+                    image: AssetImage('assets/icons/diamond.png'), height: 120),
+              ),
+            ),
+            SizedBox(
+              height: 70,
+            ),
             Text('TeamUP',
                 style: TextStyle(
                   fontSize: 40,
@@ -35,17 +43,21 @@ class LoginPage extends StatelessWidget {
                   'LOG IN',
                   style: TextStyle(color: Colors.white),
                 ),
-                onPressed: () {},
+                onPressed: () {
+                  Navigator.pushNamed(context, loginScreen);
+                },
               ),
               FlatButton(
                 child: Text(
                   'SIGN UP',
                   style: TextStyle(color: Colors.white),
                 ),
-                onPressed: () {},
+                onPressed: () {
+                  Navigator.pushNamed(context, signUp);
+                },
               )
             ],
-          )),
+          ),),
     );
   }
 }
